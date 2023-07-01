2f4f1881364562d4e915f760ad0ad6d3c7c81470
Abhishek Singh Rana <abhisingh.72340@gmail.com>
2023-07-01 13:51:22 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ export default function NavBar() {
                 <div className="social">
                     <ul id='media'>
                         <li> <a href='https://www.instagram.com/__abhisheksinghrana/'>instagram</a></li>
-                        <li><a href='https://www.linkedin.com/in/abhishek-singh-rana-6354a9180/'>likedin</a></li>
+                        <li><a href='https://www.linkedin.com/in/abhishek-singh-rana-6354a9180/'>linkedin</a></li>
                         <li> <a href='https://github.com/abhishek72340?tab=repositories'>github</a></li>
                     </ul>
                 </div>
